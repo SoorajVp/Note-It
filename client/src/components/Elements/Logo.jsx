712ad5f43becf25f5b97e35304dfa71cd3b48b3e
Sooraj Vp <soorajvp2017@gmail.com>
2023-12-20 11:34:11 +0530

    Name changed

--- client/src/components/Elements/Logo.jsx
+++ client/src/components/Elements/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <div className=' flex text-white'>
             <img src={logo} alt="logo" className='w-9 h-9' />
-            <h1 className='text-2xl font-semibold border-t-2 border-r-2 border-b-2 pr-2'>Note-It</h1>
+            <h1 className='text-2xl font-semibold border-t-2 border-r-2 border-b-2 pr-2 uppercase'>BytePad</h1>
         </div>
     )
 }
